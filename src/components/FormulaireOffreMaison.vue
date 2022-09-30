8ba3fcbd517602d5a9a29fd58de86218a57fed56
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2022-09-30 15:20:44 +0200

    retouche formulaire

--- src/components/FormulaireOffreMaison.vue
+++ src/components/FormulaireOffreMaison.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
     import { ref } from "@vue/reactivity";
-import card from "./card.vue";
+    import card from "./card.vue";
     // On fait une variable réactive qui réference les données
     // ATTENTION : faire une ref pas une Reactive car :
     // c'est l'objet qui doit être réactif, pas ses props
@@ -16,18 +16,15 @@ import card from "./card.vue";
                     Résultat (Prévisualisation)
                 </h2>
                 <!--Optionnel on affiche les données-->
-                <Card v-bind="maison" />
+                <card v-bind="maison" />
             </div>
             <div class="p-2">
                 <!-- On passe la "ref" à FormKit-->
-                <FormKit type="form" v-model="maison" 
-                :config="{
-                classes: {
-                input: 'p-1 rounded border-gray-300 shadow-sm border',
-                label: 'text-gray-600',
-                },
-                }"
-                >
+                <FormKit type="form" @submit="upsertMaison" v-model="maison" :config="{classes: 
+                {input: 'p-1 rounded border-gray-300 shadow-sm border',label: 'text-gray-600',},}" 
+                :submit-attrs="{ classes:
+                { input: 'bg-red-300 p-1 rounded' } }">
+                
                     <FormKit name="titre" label="titre" />
                     <FormKit name="price" label="prix" type="number" />
                     <FormKit name="image" label="image" />
