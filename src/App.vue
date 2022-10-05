f7ef50d29e8e7d85c682e3eb15de1dd50848c07b
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2022-10-05 14:20:24 +0200

    ajout login/logout

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,11 @@
             <code class="font-mono">listsupabase</code>
           </router-link>
       </li>
-   
+      <li>
+      <router-link class="text-red-600 underline" to="/login-logout">
+        Login/logout
+      </router-link>
+      </li>
     </ul>
   </nav>
 
